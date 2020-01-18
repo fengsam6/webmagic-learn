3537ff62bd2cea7d4a6e5487a8a0f10862cc7791
feng <1073612350@qq.com>
2020-01-18 23:57:51 +0800

    暂时关闭定时功能

--- src/main/java/com/feng/WebmagicLearnApplication.java
+++ src/main/java/com/feng/WebmagicLearnApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling
 @EnableAsync
 public class WebmagicLearnApplication {
 
